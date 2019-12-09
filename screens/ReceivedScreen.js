2abb58b9d6a80b96d90821135ba8789c8a4f64ef
Pierce Morris <pierce.morris1998@gmail.com>
2019-12-09 16:42:20 +0000

    commit

--- screens/ReceivedScreen.js
+++ screens/ReceivedScreen.js
@@ -2,11 +2,20 @@ import React from "react";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 
 import Header from "../components/UI/Header";
+import Subsection from "../components/UI/Subsection";
+
+const data = [
+  { name: "Pierce", tour: "Japanese Adventure" },
+  { name: "Pierce", tour: "Japanese Adventure" },
+  { name: "Pierce", tour: "Japanese Adventure" }
+];
 
 const ReceivedScreen = () => {
   return (
     <SafeAreaView>
       <Header main title="Received Gifts" />
+      <Subsection text="Ready to embark" />
+      <Subsection text="Recent friends" />
     </SafeAreaView>
   );
 };
